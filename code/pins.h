2943fe380c1ec12b0ce93479f97f379aad7f72b8
czkme <czkme@foxmail.com>
2026-06-12 02:42:59 +0800

    Build C3 and S3 release firmware

--- code/pins.h
+++ code/pins.h
@@ -1,11 +1,26 @@
-﻿#pragma once
+#pragma once
 
-// ESP32-S3 SuperMini 串口和模块控制引脚
+// Build with -DSMS_BOARD_C3 or -DSMS_BOARD_S3.
+#if defined(SMS_BOARD_C3)
+// ESP32-C3 SuperMini UART and modem control pins.
+#define TXD 3
+#define RXD 4
+#define MODEM_EN_PIN 5
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 8
+#endif
+
+#elif defined(SMS_BOARD_S3)
+// ESP32-S3 SuperMini UART and modem control pins.
 #define TXD 43
 #define RXD 44
 #define MODEM_EN_PIN 5
 
-// ESP32-S3 SuperMini 板载RGB LED常用GPIO
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 48
+#endif
+
+#else
+#error "Please define SMS_BOARD_C3 or SMS_BOARD_S3"
 #endif
